5b418ac0c4010e7f18c32af0ea30cf6f732d139c
HansonLau <shadowguy368@gmail.com>
2021-02-21 18:17:16 -0800

    updated

---
+++ probabiltySamplingDistribution.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import random
+
+
+proportion = float(input("Enter proportion ex: .13 (INCLUDE DECIMAL): "))
+sampleSize = int(input("Enter sample size ex: 200 : "))
+simulations = int(input("Enter numner of simulations ex: 100 : "))
+
+bigList = [0]*100
+
+def generateList(p, l):
+    temp = int(p*100)
+    for i in range(temp):
+        l[i] = 1
+    return l
+
+def simulate(l, s):
+    total = []
+    for i in range(s):
+        x = random.randint(0,99)
+        total.append(l[x])
+    
+    return sum(total)/s
+    
+
+
+
+newList = generateList(proportion, bigList)
+
+data = []
+
+for i in range(simulations):
+    data.append(simulate(newList, sampleSize))
+
+print(data)
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
-for i in range(2):
-    print("a")
-    i = 0
+import random
+for i in range(10):
+    print(random.randint(1,2))
